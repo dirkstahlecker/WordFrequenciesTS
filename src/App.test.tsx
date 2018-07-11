8fccbc95d71bb0575e816e2ed1c5d292332381b6
Dirk <stahdirk@mit.edu>
2018-07-11 00:03:01 -0400

    added mobx; minor work on layout; started venturing into machine

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import {App, AppMachine} from './App';
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App machine={new AppMachine()}/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,62 @@
 import * as React from 'react';
-import * as $ from 'jquery';
-
+// import * as $ from 'jquery';
+import {NameReference} from "./NameReference";
+import {observable, runInAction} from "mobx";
+import {observer} from "mobx-react";
+// import Popup from "reactjs-popup";
 // import logo from './logo.svg';
 
-export class App extends React.Component 
+export class AppMachine
 {
-  public onSubmit = (): void => {
-    const entry: any = $("#journalEntry").val();
-    console.log(entry);
+  @observable
+  public journalText: string;
+
+  @observable
+  public currentName: string;
+}
+
+export interface AppProps
+{
+  machine: AppMachine;
+}
+
+@observer
+export class App extends React.Component<AppProps>
+{
+  public onSubmit = (): void => {    
+    for (let word in this.props.machine.journalText.split(" "))
+    {
+      if (NameReference.isName(word))
+      {
+        runInAction(() => {
+          this.props.machine.currentName = word;
+        });
+      }
+    }
   };
 
   public render() 
   {
     return (
-      <>
-        <label htmlFor="dateEntry">Date: </label>
-        <br />
-        <input type="text" id="dateEntry" />
-        <br />
-        <br />
-        <label htmlFor="journalEntry">Entry: </label>
-        <br />
-        <textarea id="journalEntry" />
-        <br />
-        <button onClick={this.onSubmit}>Submit</button>
-      </>
+      <span style={{width: "100%", display: "inline-block", verticalAlign: "top"}}>
+        <div style={{width: "50%", display: "inline-block", verticalAlign: "top"}}>
+          <label htmlFor="dateEntry">Date: </label>
+          <br />
+          <input type="text" id="dateEntry" />
+          <br />
+          <br />
+          <label htmlFor="journalEntry">Entry: </label>
+          <br />
+          <textarea id="journalEntry" value={this.props.machine.journalText}/>
+          <br />
+          <button onClick={this.onSubmit}>Submit</button>
+        </div>
+        <div style={{width: "50%", display: "inline-block", verticalAlign: "top"}}>
+          {}
+          <button>Okay</button>
+          <button>Ignore</button>
+        </div>
+      </span>
     );
   }
 }
